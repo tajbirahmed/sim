994be05e23b26e1c90242b76f49e89b381cf7c06
tajbirahmed <tajbir26@gmail.com>
2024-03-07 00:01:00 +0600

    added navbar, footer

---
+++ components/NavBarComponents/NavBar.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import { NavData } from '@/constants/NavCompData'
+import Image from 'next/image'
+import Link from 'next/link'
+import NavComp from './NavComp'
+
+// 1.cofigure text color, bg color, height, 
+// 2, profile 
+
+const NavBar = () => {
+    return (
+        <nav className="h-10 w-full flex flex-row justify-between items-center px-3">
+            <Link
+                href="/"
+            >
+                <div className='flex flex-row justify-center items-center hover:text-slate-500'>
+                    <div className=''>
+                        
+                            <Image
+                                src="/assets/cu-logo.png"
+                                width={27}
+                                height={80}
+                                alt='logo'
+                            />
+                        
+                    </div>
+                    <div className='px-2 pl-3 font-semibold text-[22px]'>
+                        <p>University of Chittagong</p>
+                    </div>
+                </div>  
+            </Link>
+            <div className='flex flex-row justify-center align-middle '>
+                {NavData.map((item, index) => (
+                    <NavComp
+                        key={index}
+                        title={item.title}
+                        link={ item.link }
+                    />
+                ))}
+                {/* // fix this */}
+                <Link href="/">
+                    <div className='px-5 hover:bg-gray-100 rounded-full'>
+                        <div className='container mx-auto px-2'>
+                            <p className='font-bold text-slate-900 hover:text-slate-500'>
+                                profile
+                            </p>
+                        </div>
+                    </div>
+                </Link>
+            </div>
+        </nav>
+
+    )
+}
+
+export default NavBar
---
+++ constants/NavCompData.ts
@@ -0,0 +1,22 @@
+export const NavData = [
+  {
+    title: "Semster", 
+    link: "/semester"
+  }, 
+  {
+    title: "Enrolled Courses",
+    link: "/enrolled-courses"
+  },
+  {
+    title: "Events",
+    link: "/events"
+  },
+  {
+    title: "Submit Application",
+    link: "/submit-application"
+  },
+  {
+    title: "Result",
+    link: "/result"
+  },
+]
