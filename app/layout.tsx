966f963b5e28a7af9473235ff53f4f5a0606cf5e
tajbirahmed <tajbir26@gmail.com>
2024-03-06 23:55:09 +0600

    added navbar, footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,29 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/NavBarComponents/NavBar";
+import FooterBar from "@/components/FooterComponents/FooterBar";
 
 const inter = Inter({ subsets: ["latin"] });
-
+// 1. font selection. 
 export const metadata: Metadata = {
-  title: "Student Information Management",
+    title: "Student Information Management",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <NavBar />
+                {children}
+                <div className="flex-end">
+                    <FooterBar />
+                </div>
+            </body>
+        </html>
+    );
 }
