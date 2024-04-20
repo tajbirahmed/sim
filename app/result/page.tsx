c0007faff1098c1883d5adc8a0bdc6fcdee892c5
Ashraf1292 <ifteekhar1292@gmail.com>
2024-04-20 22:10:20 +0600

    newest commit

--- app/result/page.tsx
+++ app/result/page.tsx
@@ -7,6 +7,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
+import { ChevronDown } from 'lucide-react';
+
 interface ButtonProps {
   children: ReactNode;
   className?: string;
@@ -14,8 +16,8 @@ interface ButtonProps {
 
 const Button = ({ children, className }: ButtonProps) => {
   return (
-    <button className={`text-xl font-semibold h-[40px] w-[180px] rounded-xl hover:bg-gray-400 hover:text-white ${className}`}>
-      {children}
+    <button className={`text-xl font-semibold h-12 w-48 rounded-xl hover:bg-gray-400 hover:text-white flex items-center justify-between px-4 ${className}`}>
+      <span>{children}</span>
     </button>
   );
 };
@@ -37,13 +39,19 @@ const Result = () => {
       <div className="ml-10">
         <p className="font-semibold text-xl pb-4 pt-10">Please select a semester from the dropdown box to see your results:</p>
       </div>
-      
       <DropdownMenu>
-        <DropdownMenuTrigger className="font-semibold text-xl">Choose Semester</DropdownMenuTrigger>
+        <div className="relative inline-block">
+          <DropdownMenuTrigger className="font-semibold text-xl border-2 border-gray-300 rounded-md px-8 py-2 cursor-pointer pr-8">
+            Choose Semester
+            <ChevronDown className="text-gray-600 absolute top-1/2 right-0 transform -translate-y-1/2 mr-2" />
+          </DropdownMenuTrigger>
+        </div>
         <DropdownMenuContent className="pl-10 justify-center font-semibold text-2xl w-48">
           {semesters.map((semester, index) => (
             <Link href={semester.link} key={index}> {/* Wrap DropdownMenuItem in Link */}
-              <DropdownMenuItem>{semester.name}</DropdownMenuItem>
+              <DropdownMenuItem className="hover:bg-gray-400"> {/* Add hover:bg-gray-400 class */}
+                {semester.name}
+              </DropdownMenuItem>
             </Link>
           ))}
         </DropdownMenuContent>
