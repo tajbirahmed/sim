8b49c2be80b07910f962282e02006e3aa6f19c89
tajbirahmed <tajbir26@gmail.com>
2024-04-23 16:04:13 +0600

    current working

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,15 +1,16 @@
 "use client";
 import { Home, HomeIcon, Table2, LayoutGrid, LayoutDashboard, } from 'lucide-react'
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import SideBarComp from './SideBarComp';
 import ProjectInfoComp from './ProjectInfoComp';
 import ProfileComp from './ProfileComp';
+import { SideBarContext } from '@/contexts/SideBarContext';
 
 
 
 const SideBar = () => {
-  const [selected, setSelected] = useState('Home');
+  const { content, setContent } = useContext(SideBarContext)!;
   
   return (
     <div className="h-[92vh] min-w-44 overflow-y-hidden sticky flex flex-col shadow-md shadow-slate-500 dark:shadow-slate-800 rounded-r-xl
@@ -21,33 +22,33 @@ const SideBar = () => {
         title='Home'
         link="/"
         icon={<HomeIcon size={ 15 } className='mt-[4px]' /> }
-        selected={selected}
-        setSelected={setSelected}
+        selected={content}
+        setSelected={setContent}
       />
       <SideBarComp
         title='Dashboard'
         link='/dashboard'
         icon={<LayoutDashboard size={15} className='mt-[4px]' />}
-        selected={selected}
-        setSelected={setSelected}
+        selected={content}
+        setSelected={setContent}
       />
       <SideBarComp
         title='Semester'
         link='/semester'
-        selected={selected}
-        setSelected={setSelected}
+        selected={content}
+        setSelected={setContent}
       />
       <SideBarComp
         title='Course'
-        link='/enrolled-courses'
-        selected={selected}
-        setSelected={setSelected}
+        link='/result'
+        selected={content}
+        setSelected={setContent}
       />
       <SideBarComp
         link='/result'
         title='Result'
-        selected={selected}
-        setSelected={setSelected}
+        selected={content}
+        setSelected={setContent}
       />
       <div className='flex-1 flex flex-col justify-end'>
         <ProfileComp
