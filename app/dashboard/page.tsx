d8b1f2beee04ea8f9d32d97971cac67dadbdb0cd
papanbanik <papanbanik58@gmail.com>
2024-07-07 22:59:01 +0600

    event delete

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,16 +7,12 @@ import NavigateComp from "@/components/NavigateComp";
 const Dashboard = () => {
   return (
     <div className="w-full flex flex-col h-[94vh] overflow-y-auto no-scrollbar pt-6 ml-5">
-      <NavigateComp
-        title="Dashboard"
-        profile={true}
-        dashboard={true}
-      />
+      <NavigateComp title="Dashboard" profile={true} dashboard={true} />
       <p className="text-lg font-bold text-gray-800 self-center mr-96">
         Welcome, <span className="text-blue-500">John Doe!</span>
       </p>
 
-      <div className="grid grid-cols-3 gap-0 self-center">
+      <div className="grid grid-cols-3 gap-20 pl-52">
         {/* Semester */}
         <div className="card bg-slate-100 flex-row p-8">
           <h2 className="title">Semester</h2>
@@ -34,18 +30,12 @@ const Dashboard = () => {
         </div>
 
         {/* Upcoming Events */}
-        <div className="card bg-slate-100  p-7 ">
-          <h2 className="title ">Upcoming Events</h2>
-          <p className="text-gray-600">
-            <span className="text-indigo-500">2 New Events</span>
-          </p>
-        </div>
       </div>
       <div className="ml-40 mt-0">
         <BarChart />
       </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
